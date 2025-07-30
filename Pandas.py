9e619ab4f0fa73508f6c6b3b7f6fe19ab940d04a
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-07-31 02:33:23 +0300

    pandas part1

---
+++ Pandas.py
@@ -0,0 +1,110 @@
+# Basic 
+import pandas as pd 
+dict = {
+    
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
+}
+x = pd.DataFrame(dict) 
+print(x)
+print("\n \n")
+#---------------------------------
+#  change index for your data frame  
+import pandas as pd 
+dict = {
+    
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
+}
+
+x = pd.DataFrame(dict) 
+x.index = {"BR","RU","IN","CH","SA"} # change index for your data frame
+print(x)
+print("\n \n")
+
+#---------------------------------
+#create csv file to store the data 
+
+import pandas as pd 
+dict = {
+    
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
+}
+
+df = pd.DataFrame(dict)
+df.to_csv("Country.csv")
+
+#---------------------------------
+
+#read form csv file 
+import pandas as pd 
+
+df = pd.read_csv("Country.csv")
+print(df)
+print("\n \n")
+#---------------------------------
+# Edit index 
+
+#read form csv file 
+import pandas as pd 
+
+df = pd.read_csv("Country.csv" , index_col= 0)
+print(df)
+print("\n \n")
+
+# ------------------------------------
+
+# To access on data
+import pandas as pd
+dict = {
+    
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
+}
+
+df = pd.DataFrame(dict)
+
+print(df["country"]) # return series not data frame 
+print(df[["country"]]) # return DataFrame 
+
+print(df[1:4]) # row 1 , 2 and 3 and their columns 
+
+# ------------------------------------
+# To access on data using loc and iloc 
+
+import pandas as pd
+dict = {
+    
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
+}
+
+df = pd.DataFrame(dict)
+df.index = {"BR","RU","IN","CH","SA"} 
+
+
+
+print(df.loc["BR"]) # return BR row as series 
+print(df.loc[["BR"]]) # return BR row as DataFrame 
+print(df.loc[["BR" , "SA"]]) # return BR and SA row as DataFrame 
+
+
+print(df.loc['RU' , ["area"]]) # return area for RU as a series 
+print(df.loc[['RU'] , ["area"]]) # return area for RU as a DataFrame 
+
+
+print(df.loc[:,["country" , "area"]]) # return country and area for all data as DataFrame 
+print(df.loc[["RU"],:]) # return all column for RU as DataFrame 
+
+# -------------------------------------------------------------------
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,25 @@
+import pandas as pd
+dict = {
+    
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
+}
 
-x = "ahmed hussein"
-print(x.center(15)) # it see the length of the string and if the prameter is larger it will be move string to center it 
+df = pd.DataFrame(dict)
+df.index = {"BR","RU","IN","CH","SA"} 
 
-print(x.count("s")) # Count something in string 
-print(x.encode())
+
+
+# print(df.loc["BR"]) # return BR row as series 
+# print(df.loc[["BR"]]) # return BR row as DataFrame 
+# print(df.loc[["BR" , "SA"]]) # return BR and SA row as DataFrame 
+
+
+# print(df.loc['RU' , ["area"]]) # return area for RU as a series 
+# print(df.loc[['RU'] , ["area"]]) # return area for RU as a DataFrame 
+
+
+# print(df.loc[:,["country" , "area"]]) # return country and area for all data as DataFrame 
+print(df.loc[["RU"],:]) # return country and area for all data as DataFrame 
