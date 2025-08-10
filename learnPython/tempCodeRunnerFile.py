babcf2eb1f7b51118b2b20cb0c4a5585dcaebd94
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-08-10 21:29:30 +0300

    basic Json(loads(parse) , dumps)

--- learnPython/tempCodeRunnerFile.py
+++ learnPython/tempCodeRunnerFile.py
@@ -1,7 +1,11 @@
+import json
 
-import datetime
-
-x = datetime.datetime.now()
-
-print(x.year)
-print(x.strftime("%B"))
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
