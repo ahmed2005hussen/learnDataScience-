03b61b39f18685a8e3adb43eb4d1b01bf7a8e95d
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-08-14 15:57:48 +0300

    Merging Tables With Different Join Types

--- packagesForDataScience/Pandas_part3.py
+++ packagesForDataScience/Pandas_part3.py
@@ -4,7 +4,7 @@
 # Tables = DataFrames 
 # Merging = Joining 
 
-# Inner Join
+# Inner Join -> هيرجع المشترك بين الاتنين
 import pandas as pd 
 feb = pd.read_csv(r"Joining Data With Pandas\sales_feb.csv")
 jan = pd.read_csv(r"Joining Data With Pandas\sales_jan.csv")
@@ -43,3 +43,49 @@ print(jan['ProductName'].value_counts())
 # To merge Three tables
 
 # table1.merge(table2 , on = [col1,col2,..]).merge(table3,on = col1)
+
+# ---------------------------
+# Left Join -> هيرجع اللي في الشمال كله و لو في حاجه من اليمين موجوده في الشمال 
+
+import pandas as pd
+students = pd.read_csv(r"Joining Data With Pandas\students.csv")
+grades = pd.read_csv(r"Joining Data With Pandas\grades.csv")
+
+left_join = students.merge(grades , on = "StudentID"  , how = "left")
+print(left_join)
+
+# Right Join -> عكس ال left
+
+import pandas as pd
+students = pd.read_csv(r"Joining Data With Pandas\students.csv")
+grades = pd.read_csv(r"Joining Data With Pandas\grades.csv")
+
+right_join = students.merge(grades , on = "StudentID"  , how = "right")
+print(right_join)
+
+# outer join هيرجع كله اليمين و الشمال
+
+import pandas as pd
+students = pd.read_csv(r"Joining Data With Pandas\students.csv")
+grades = pd.read_csv(r"Joining Data With Pandas\grades.csv")
+
+outer_join = students.merge(grades , on = "StudentID"  , how = "outer")
+print(outer_join)
+
+# if the name of "on columns" different u can use 
+
+# table1.merge(table2 , left_on = nameColumn , right_on = nameColumn )
+
+# ----------------------------
+# self join 
+# Self Join is when a table is joined with itself.
+
+# It’s used when you need to relate rows within the same table 
+# based on a condition.
+
+# table.merge(table , left_on = namecol , right_on = nameCol)
+
+# -----------------------------
+# Merging on index يعني هنخلي عمود هو الاندكس بعد ال merge
+
+# table1.merge(table2 , left_on = namecol , right_on = nameCol , right_index = True)
--- packagesForDataScience/tempCodeRunnerFile.py
+++ packagesForDataScience/tempCodeRunnerFile.py
@@ -1,5 +1,7 @@
-import pandas as pd 
 
-jan = pd.read_csv(r"Joining Data With Pandas\sales_jan.csv")
+import pandas as pd
+students = pd.read_csv(r"Joining Data With Pandas\students.csv")
+grades = pd.read_csv(r"Joining Data With Pandas\grades.csv")
 
-print(jan['ProductName'].value_counts())
+outer_join = students.merge(grades , on = "StudentID"  , how = "outer")
+print(outer_join)
