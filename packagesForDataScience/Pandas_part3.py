e71900713a7a5f4b999f3200bf552550c37d4b8d
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-08-15 21:07:04 +0300

    joining data

--- packagesForDataScience/Pandas_part3.py
+++ packagesForDataScience/Pandas_part3.py
@@ -4,7 +4,7 @@
 # Tables = DataFrames 
 # Merging = Joining 
 
-# Inner Join -> هيرجع المشترك بين الاتنين
+# Inner Join -> هيرجع المشترك بين الاتنين -> Default 
 import pandas as pd 
 feb = pd.read_csv(r"Joining Data With Pandas\sales_feb.csv")
 jan = pd.read_csv(r"Joining Data With Pandas\sales_jan.csv")
@@ -324,4 +324,190 @@ orders = pd.DataFrame({
 result = pd.concat([customers,orders] , verify_integrity = False)
 print(result)
 
-# ---------------
+# ---------------------
+
+# Merge_ordered() -> outer join is default 
+# use it when u work on ordered data / time series 
+import pandas as pd
+
+stock_a = pd.DataFrame({
+    'date': ['2023-01-01', '2023-01-03', '2023-01-04'],
+    'price_a': [100, 102, 103]
+})
+
+stock_b = pd.DataFrame({
+    'date': ['2023-01-02', '2023-01-03', '2023-01-05'],
+    'price_b': [200, 202, 205]
+})
+
+merged = pd.merge_ordered(stock_a, stock_b, on='date')
+
+print(merged)
+
+# filling missing value with previous value
+
+import pandas as pd
+
+stock_a = pd.DataFrame({
+    'date': ['2023-01-01', '2023-01-03', '2023-01-04'],
+    'price_a': [100, 102, 103]
+})
+
+stock_b = pd.DataFrame({
+    'date': ['2023-01-02', '2023-01-03', '2023-01-05'],
+    'price_b': [200, 202, 205]
+})
+
+merged = pd.merge_ordered(stock_a, stock_b, on='date' , fill_method= "ffill")
+
+print(merged)
+
+# -------------------------------
+# Using merge_asof()
+
+# هي زي -> left join 
+#لكن مش بتعمل المطابقة بالقيمة نفسها
+# بتختار أقرب قيمة سابقة أو مساوية في العمود اللي بنعمل عليه الدمج.
+
+
+# Left Table                # Right Table
+
+# | B  | C  |               | C | D  |
+# | -- | -- |               | - | -- |
+# | B2 | 1  | - - - - - - ->| 1 | D1 |
+# | B3 | 5  | - - -         | 2 | D2 |
+# | B4 | 10 | - -  | - - - >| 3 | D3 |  
+#                |          | 6 | D6 |
+#                | - - - - >| 7 | D7 |
+
+
+#  Result
+ 
+# | B  | C  | D  |
+# | -- | -- | -- |
+# | B2 | 1  | D1 |
+# | B3 | 5  | D3 |
+# | B4 | 10 | D7 |
+
+
+#  شروط merge_asof():
+
+# لازم العمود اللي بتعمل عليه الدمج  يكون مرتب تصاعديًا.
+
+# بيشتغل زي -> merge_ordered() left join
+#  لكن بالمطابقة الأقرب بدل المطابقة الدقيقة.
+
+# مفيد جدًا في دمج البيانات الزمنية - > (time series)
+# مثلاً تربط أسعار الأسهم بأقرب تاريخ متاح.
+
+
+import pandas as pd
+
+left = pd.DataFrame({
+    'B': ['B2', 'B3', 'B4'],
+    'C': [1, 5, 10]
+})
+
+right = pd.DataFrame({
+    'C': [1, 2, 3, 6, 7],
+    'D': ['D1', 'D2', 'D3', 'D6', 'D7']
+})
+
+left = left.sort_values('C')
+right = right.sort_values('C')
+
+result = pd.merge_asof(left, right, on='C', direction='backward')
+# direction='forward' → هيجيب أقرب قيمة أكبر أو مساوية.
+# direction='nearest' → هيجيب أقرب قيمة سواء أصغر أو أكبر حسب المسافة.
+# direction='backward' → هيجيب أقرب قيمة أصغر أو مساوية.
+
+print(result)
+
+# ----------------------------
+
+import pandas as pd
+
+data = {
+    "Name": ["Ahmed", "Sara", "Omar", "Laila", "Mona"],
+    "Age": [23, 30, 19, 25, 28],
+    "City": ["Cairo", "Giza", "Cairo", "Alexandria", "Giza"]
+}
+df = pd.DataFrame(data)
+
+result1 = df.query("Name == 'Ahmed'")
+print(result1)
+
+result1 = df.query("Age == 30")
+print(result1)
+
+result1 = df.query("Age < 30 and City ==  'Giza' ")
+print(result1)
+
+# -------------------------------
+
+# Reshaping data with .melt()
+
+
+# .melt() in Pandas is used to reshape your DataFrame from a wide format
+# to a long format.
+
+# Wide format → each variable has its own column.
+
+# Long format → all variable values are stacked in a single column,
+# with another column indicating which variable they came from.
+
+# It’s very useful for data cleaning and preparing your dataset for analysis
+# or visualization.
+
+# Before .melt()
+
+# | Name | Math | Science | English |
+# | ---- | ---- | ------- | ------- |
+# | Ali  | 90   | 85      | 88      |
+# | Sara | 78   | 92      | 80      |
+# | Omar | 85   | 89      | 94      |
+
+# After .melt()
+
+# | Name | Subject | Score |
+# | ---- | ------- | ----- |
+# | Ali  | Math    | 90    |
+# | Sara | Math    | 78    |
+# | Omar | Math    | 85    |
+# |------|---------|-------|
+# | Ali  | Science | 85    |
+# | Sara | Science | 92    |
+# | Omar | Science | 89    |
+# |------|---------|-------|
+# | Ali  | English | 88    |
+# | Sara | English | 80    |
+# | Omar | English | 94    |
+
+
+import pandas as pd
+
+data = {
+    'Name': ['Ali', 'Sara', 'Omar'],
+    'Math': [90, 78, 85],
+    'Science': [85, 92, 89],
+    'English': [88, 80, 94]
+}
+
+df = pd.DataFrame(data)
+print("📊 قبل melt:")
+print(df)
+print("\n\n")
+
+
+# id_vars -> الأعمدة اللي هتفضل زي ما هي
+# value_vars -> الأعمدة اللي هتحولها لصفوف
+# var_name -> اسم العمود الجديد اللي هيحتوي أسماء الأعمدة القديمة
+# value_name ->  اسم العمود الجديد اللي هيحتوي القيم
+
+df_melted = pd.melt(df, id_vars=['Name'], 
+                    value_vars=['Math', 'Science', 'English'], 
+                    var_name='Subject', 
+                    value_name='Score')
+
+print("\n📉 بعد melt:")
+print(df_melted)
--- packagesForDataScience/tempCodeRunnerFile.py
+++ packagesForDataScience/tempCodeRunnerFile.py
@@ -1,5 +1,28 @@
-name = "ahmed gmail.com"
-if name.endswith("@gmail.com"):
-    print("yes")
-else : 
-    print("No")
+
+import pandas as pd
+
+data = {
+    'Name': ['Ali', 'Sara', 'Omar'],
+    'Math': [90, 78, 85],
+    'Science': [85, 92, 89],
+    'English': [88, 80, 94]
+}
+
+df = pd.DataFrame(data)
+print("📊 قبل melt:")
+print(df)
+print("\n\n")
+
+
+# id_vars -> الأعمدة اللي هتفضل زي ما هي
+# value_vars -> الأعمدة اللي هتحولها لصفوف
+# var_name -> اسم العمود الجديد اللي هيحتوي أسماء الأعمدة القديمة
+# value_name ->  اسم العمود الجديد اللي هيحتوي القيم
+
+df_melted = pd.melt(df, id_vars=['Name'], 
+                    value_vars=['Math', 'Science', 'English'], 
+                    var_name='Subject', 
+                    value_name='Score')
+
+print("\n📉 بعد melt:")
+print(df_melted)
