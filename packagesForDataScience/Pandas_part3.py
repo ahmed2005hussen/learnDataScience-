0698471aa9bd221e06d14d4a15b9fa6c4ec0b729
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-08-15 04:16:46 +0300

    Advanced Merging and Concatenating

--- packagesForDataScience/Pandas_part3.py
+++ packagesForDataScience/Pandas_part3.py
@@ -89,3 +89,239 @@ print(outer_join)
 # Merging on index يعني هنخلي عمود هو الاندكس بعد ال merge
 
 # table1.merge(table2 , left_on = namecol , right_on = nameCol , right_index = True)
+
+# ---------------------------------------------
+
+# Semi Join -> بيجيب لك الصفوف من الجدول الأول اللي ليها تطابق في الجدول التاني،
+# لكن ما بيجيبش الأعمدة من الجدول التاني.
+
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [1, 2, 3],
+    'name': ['Ali', 'Sara', 'Omar']
+})
+
+orders = pd.DataFrame({
+    'order_id': [1001, 1002],
+    'customer_id': [1, 3]
+})
+
+innerJoin = customers.merge(orders , on = "customer_id" , how = "inner")
+semi_join = customers[customers["customer_id"].isin(innerJoin["customer_id"])]
+print(semi_join)
+
+# OR
+
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [1, 2, 3],
+    'name': ['Ali', 'Sara', 'Omar']
+})
+
+orders = pd.DataFrame({
+    'order_id': [1001, 1002],
+    'customer_id': [1, 3]
+})
+
+semi_join = customers[customers["customer_id"].isin(orders["customer_id"])]
+print(semi_join)
+# ---------------------------------
+# Anti Join -> Returns The Left table, excluding the intersection 
+# Returns only columns from the left table not the right 
+
+
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [1, 2, 3],
+    'name': ['Ali', 'Sara', 'Omar']
+})
+
+orders = pd.DataFrame({
+    'order_id': [1001, 1002],
+    'customer_id': [1, 3]
+})
+
+left_join = customers.merge(orders , on = "customer_id" , how = "left" , indicator= True) 
+# indicator -> add column named _merge , this column tell us if this row from left or right or both tables 
+from_left_only = left_join.loc[left_join["_merge"] == "left_only"]
+anti_merge = customers[customers["customer_id"].isin(from_left_only["customer_id"])]
+print(anti_merge)
+
+# OR 
+
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [1, 2, 3],
+    'name': ['Ali', 'Sara', 'Omar']
+})
+
+orders = pd.DataFrame({
+    'order_id': [1001, 1002],
+    'customer_id': [1, 3]
+})
+
+# Anti join: العملاء اللي ما عندهمش طلبات
+anti_join = customers[~customers['customer_id'].isin(orders['customer_id'])]
+
+print(anti_join)
+
+# --------------------------
+
+# Concatenate DataFrames together vertically
+
+import pandas as pd
+
+inv_jan = pd.DataFrame({
+    'product': ['A', 'B'],
+    'sales': [100, 150]
+})
+
+inv_feb = pd.DataFrame({
+    'product': ['C', 'D'],
+    'sales': [200, 250]
+})
+
+inv_mar = pd.DataFrame({
+    'product': ['E', 'F'],
+    'sales': [300, 350]
+})
+
+result = pd.concat([inv_jan , inv_feb ,inv_mar])
+print(result)
+# look on index we have something wrong
+# to solve it use (ignore_index = True)
+
+import pandas as pd
+
+inv_jan = pd.DataFrame({
+    'product': ['A', 'B'],
+    'sales': [100, 150]
+})
+
+inv_feb = pd.DataFrame({
+    'product': ['C', 'D'],
+    'sales': [200, 250]
+})
+
+inv_mar = pd.DataFrame({
+    'product': ['E', 'F'],
+    'sales': [300, 350]
+})
+
+result = pd.concat([inv_jan , inv_feb ,inv_mar] , ignore_index= True)
+print(result)
+
+# Setting labels to original tables
+
+import pandas as pd
+
+inv_jan = pd.DataFrame({
+    'product': ['A', 'B'],
+    'sales': [100, 150]
+})
+
+inv_feb = pd.DataFrame({
+    'product': ['C', 'D'],
+    'sales': [200, 250]
+})
+
+inv_mar = pd.DataFrame({
+    'product': ['E', 'F'],
+    'sales': [300, 350]
+})
+
+result = pd.concat([inv_jan , inv_feb ,inv_mar] , ignore_index= False , keys=["Jan" , " Feb" , "Mar"])
+print(result)
+
+# you can sort
+
+import pandas as pd
+
+inv_jan = pd.DataFrame({
+    'product': ['A', 'B'],
+    'sales': [100, 150]
+})
+
+inv_feb = pd.DataFrame({
+    'product': ['C', 'D'],
+    'sales': [200, 250]
+})
+
+inv_mar = pd.DataFrame({
+    'product': ['E', 'F'],
+    'sales': [300, 350]
+})
+
+result = pd.concat([inv_jan , inv_feb ] , sort = True)
+print(result)
+
+
+# Join
+
+import pandas as pd
+
+inv_jan = pd.DataFrame({
+    'product': ['A', 'B'],
+    'sales': [100, 150]
+})
+
+inv_feb = pd.DataFrame({
+    'product': ['C', 'D'],
+    'sales': [200, 250]
+})
+
+inv_mar = pd.DataFrame({
+    'product': ['E', 'F'],
+    'sales': [300, 350]
+})
+
+result = pd.concat([inv_jan , inv_feb ] , join= "inner")
+print(result)
+
+# ---------------------------
+
+# Verifying integrity
+
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [1, 2, 3],
+    'name': ['Ali', 'Sara', 'Omar']
+})
+
+orders = pd.DataFrame({
+    'order_id': [1001, 1002],
+    'customer_id': [1, 1]
+})
+
+# result = customers.merge(orders , on = "customer_id" , how = "inner" , validate= "one_to_one")
+# Error because it is not one to one relation
+
+result = customers.merge(orders , on = "customer_id" , how = "inner" , validate= "one_to_many")
+print(result)
+
+# ----------------------------------
+
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [1, 2, 3],
+    'name': ['Ali', 'Sara', 'Omar']
+})
+
+orders = pd.DataFrame({
+    'order_id': [1001, 1002],
+    'customer_id': [1, 1]
+})
+
+# result = pd.concat([customers,orders] , verify_integrity= True) # check if we have duplicate 
+# Error because it is not one to one relation
+
+result = pd.concat([customers,orders] , verify_integrity = False)
+print(result)
+
+# ---------------
--- packagesForDataScience/tempCodeRunnerFile.py
+++ packagesForDataScience/tempCodeRunnerFile.py
@@ -1,7 +1,5 @@
-
-import pandas as pd
-students = pd.read_csv(r"Joining Data With Pandas\students.csv")
-grades = pd.read_csv(r"Joining Data With Pandas\grades.csv")
-
-outer_join = students.merge(grades , on = "StudentID"  , how = "outer")
-print(outer_join)
+name = "ahmed gmail.com"
+if name.endswith("@gmail.com"):
+    print("yes")
+else : 
+    print("No")
